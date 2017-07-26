0a3e0090ee152eecfbeafe4680383619b40cbb58
Webb Jamelo <webbteche@gmail.com>
2017-07-26 20:12:18 +0800

    Add files via upload

---
+++ assets/custom-script.js
@@ -0,0 +1,6 @@
+(function( $ ) {
+    $(function() {
+        // Add Color Picker to all inputs that have 'color-field' class
+        $( '#mbabc_color_picker_id' ).wpColorPicker();         
+    });
+})( jQuery );
