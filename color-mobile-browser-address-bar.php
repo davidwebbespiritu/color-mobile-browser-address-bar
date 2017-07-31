361a428d427a0cf765930d0171be00d03354e1c9
Webb Jamelo <webbteche@gmail.com>
2017-07-31 20:09:10 +0800

    Clean Code and Add readme.txt

--- color-mobile-browser-address-bar.php
+++ color-mobile-browser-address-bar.php
@@ -2,97 +2,93 @@
 /**
  * Plugin Name: Color Mobile Browser Address Bar
  * Plugin URI: https://github.com/webbteche/Color-Mobile-Browser-Address-Bar
- * Description: A Wordpress plugin that lets you add a custom color to the address bar of mobile browsers.
- * Version: 1.0.3
+ * Description: A WordPress plugin that lets you add a custom color to the address bar of mobile browsers.
+ * Version: 1.0.4
  * Author: Webb Jamelo
  * Author URI: https://github.com/webbteche
  * License: GPLv2 or later
  */
 
 // For security purposes, no direct browsing!
-if (!defined('ABSPATH')) exit;
+if ( !defined( 'ABSPATH' ) )
+    exit;
 
 // Create a sub menu on the appearance admin menu
-add_action('admin_menu', 'mobile_browser_address_bar_color_create_menu');
-function mobile_browser_address_bar_color_create_menu() {
-	add_theme_page('Color Mobile Browser Address Bar', 'Mobile Browser Address Bar Color', 'manage_options', 'm-address-bar-color', 'm_address_bar_color_function');
-	
-	add_action(	'admin_init',
-        'register_m_address_bar_color_settings'
-    );
-}
-
+add_action( 'admin_menu', 'mobile_browser_address_bar_color_create_menu' );
+function mobile_browser_address_bar_color_create_menu( )
+  {
+    add_theme_page( 'Color Mobile Browser Address Bar', 'Mobile Browser Address Bar Color', 'manage_options', 'm-address-bar-color', 'm_address_bar_color_function' );
+    add_action( 'admin_init', 'register_m_address_bar_color_settings' );
+  }
+  
 // Register settings
-function register_m_address_bar_color_settings() {
-    register_setting	(	'save-m-address-bar-color-settings',
-        'mobile_browser_address_bar_color_value'
-    );
-}
-
+function register_m_address_bar_color_settings( )
+  {
+    register_setting( 'save-m-address-bar-color-settings', 'mobile_browser_address_bar_color_value' );
+  }
+  
 // Enqueue Color picker scripts
 add_action( 'admin_enqueue_scripts', 'm_address_bar_color_add_color_picker' );
-function m_address_bar_color_add_color_picker( $hook ) {
- 
-	if( $_GET["page"] == "m-address-bar-color" ) {
-		
-		// Add the color picker css file       
-        wp_enqueue_style( 'wp-color-picker' ); 
-         
-        // Include our custom jQuery file with WordPress Color Picker dependency
-        wp_enqueue_script( 'custom-script-handle', plugins_url( 'admin/js/custom-script.js', __FILE__ ), array( 'wp-color-picker' ), false, true ); 
-	}
-}
-
+function m_address_bar_color_add_color_picker( $hook )
+  {
+    if ( 'appearance_page_m-address-bar-color' != $hook )
+        return;
+    // Add the color picker css file       
+    wp_enqueue_style( 'wp-color-picker' );
+    // Include our custom jQuery file with WordPress Color Picker dependency
+    wp_enqueue_script( 'custom-script-handle', plugins_url( 'admin/js/custom-script.js', __FILE__ ), array(
+         'wp-color-picker' 
+    ), false, true );
+  }
+  
 // Create plugin settings page
-function m_address_bar_color_function() {
-    if (!current_user_can('manage_options')) {
-        wp_die( __("Oops! You've gone too far."));
-    }
-    ?>
+function m_address_bar_color_function( )
+  {
+    if ( !current_user_can( 'manage_options' ) )
+      {
+        wp_die( __( "Oops! You've gone too far." ) );
+      }
+?>
 
-    <div class="wrap">
+	<div class="wrap">
 	<h2>Just add the color!</h2>
         <form method="post" action="options.php">
-            <?php
-            settings_fields		(	'save-m-address-bar-color-settings'
-            );
-            do_settings_sections(	'save-m-address-bar-color-settings'
-            );
+		<?php
+		settings_fields( 'save-m-address-bar-color-settings' );
+		do_settings_sections( 'save-m-address-bar-color-settings' );
+		
+		$mobile_browser_address_bar_color_value = esc_attr( get_option( 'mobile_browser_address_bar_color_value' ) );
 
-            $mobile_browser_address_bar_color_value = esc_attr(get_option('mobile_browser_address_bar_color_value'));
-            ?>
-
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Set Color</th>
-                    <td>
-					
-					<input type="text" id="mbabc_color_picker_id" name="mobile_browser_address_bar_color_value" value="<?php echo $mobile_browser_address_bar_color_value; ?>" />
-					
-					</td>
-                </tr>
+		?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Set Color</th>
+						<td>
+							<input type="text" id="mbabc_color_picker_id" name="mobile_browser_address_bar_color_value" value="
+							<?php echo $mobile_browser_address_bar_color_value;?>" />
+						</td>
+				</tr>
             </table>
-
-            <?php submit_button() ;?>
-
-        </form>
+		<?php
+		submit_button();
+?>
+		</form>
     </div>
-    <?php
-}
-
+	<?php
+  }
+  
 // add meta tag in <head>.
 add_action( 'wp_head', 'm_address_mobile_address_bar' );
-function m_address_mobile_address_bar() {
-
-	$mobile_browser_address_bar_color_value = esc_attr(get_option('mobile_browser_address_bar_color_value'));
-	
+function m_address_mobile_address_bar( )
+  {
+    $mobile_browser_address_bar_color_value = esc_attr( get_option( 'mobile_browser_address_bar_color_value' ) );
+    
 	//this is for Chrome, Firefox
-	echo '<meta name="theme-color" content="'.$mobile_browser_address_bar_color_value.'">';
-	//this is for Windows Phone
-	echo '<meta name="msapplication-navbutton-color" content="'.$mobile_browser_address_bar_color_value.'">';
-	//this is for iOS Safari
-	echo '<meta name="apple-mobile-web-app-capable" content="yes">';
-	echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
-}
-
+    echo '<meta name="theme-color" content="' . $mobile_browser_address_bar_color_value . '">';
+    //this is for Windows Phone
+    echo '<meta name="msapplication-navbutton-color" content="' . $mobile_browser_address_bar_color_value . '">';
+    //this is for iOS Safari
+    echo '<meta name="apple-mobile-web-app-capable" content="yes">';
+    echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
+  }
 ?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Color Mobile Browser Address Bar ===
+Contributors: webbteche
+Tags: mobile address bar color, theme color, meta tag for theme color, theme color on address bar
+Requires at least: 3.8
+Tested up to: 4.8
+Stable tag: 1.0.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A WordPress plugin that lets you add a custom color to the address bar of mobile browsers.
+
+== Description ==
+
+A simple plugin that generates a meta tag; specifically, adding "theme-color" meta data to the head section of your page. Which enable mobile browsers to get that data and apply it to the address bar.
+
+No more unsafe editing of your theme's header.php file. You simply follow the instructions provided and you are good to go.
+
+== Installation ==
+
+1. Download and extract plugin files to wp-content/plugin directory or just click Install Now in WordPress plugin page.
+1. Activate the plugin.
+1. Go to appearance menu > Mobile Browser Address Bar Color.
+1. Then just select the color that you want.
+1. Click save changes.
+1. Thats it! There will be no confirmation dialog but if you goto your site and view page source and search for "theme-color" or you can just test it on mobile chrome browser then you'll know it's working.
+
+PS: It didn't have a confirmation dialog because it's for future release and to lessen codes and bugs.
+If you have any problem other than that. Please contact me and I will be glad to help.
+
+== Frequently Asked Questions ==
+
+= It is not working =
+
+You're probably using different browser like Opera. For best solution try using Chrome and if still not working just contact me and I will be glad to help.
+
+== Changelog ==
+
+= 1.0.4 =
+*7/31/2017
+Initial Release
+
+== Upgrade Notice ==
+Please upgrade your WordPress later than v3.5 so that the color picker will work.
